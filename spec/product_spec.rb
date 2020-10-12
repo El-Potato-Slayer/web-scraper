b7d03e3b280f63b6a23f3ea2756a1d5e35664dec
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-10-12 15:33:20 +0200

    added more tests for the product class

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,13 +1,21 @@
 require './lib/product'
 
 describe '#initialize?' do
-  product = Product.new("Swagger Bagger", "$15000")
+  product = Product.new('Swagger Bagger', '$15000')
 
-  it "checks if name was correctly initialized to 'Swagger Bagger" do
-    expect(product.instance_variable_get(:@name)).to eq("Swagger Bagger") 
+  it "checks if name was correctly initialized to 'Swagger Bagger'" do
+    expect(product.instance_variable_get(:@name)).to eq('Swagger Bagger')
   end
 
-  it "checks if price was correctly initialized to '$15000" do
-    expect(product.instance_variable_get(:@price)).to eq("$15000") 
+  it 'checks if name was correctly initialized to a String' do
+    expect(product.instance_variable_get(:@name)).to be_a String
+  end
+
+  it "checks if price was correctly initialized to '$15000'" do
+    expect(product.instance_variable_get(:@price)).to eq('$15000')
+  end
+
+  it 'checks if price was correctly initialized to a String' do
+    expect(product.instance_variable_get(:@price)).to be_a String
   end
 end
