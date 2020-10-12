7d680749dc38ea7376c915f6b29b6edef23d3875
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-10-12 15:07:48 +0200

    added add_product method

--- bin/main.rb
+++ bin/main.rb
@@ -14,3 +14,14 @@ def get_category
   selected_page
 end
 
+def add_product(selected_page, page_number)
+  scraper = Scraper.new("#{selected_page}#{page_number}")
+  arr = []
+  product_listing = scraper.doc.css('div.product-card')
+  product_listing.each do |product|
+    title = product.css("div.product-card__title").text
+    price = "R" + product.css("span.price-item--regular").text.delete_suffix("\n").split(" ")[-1]
+    arr << Product.new(title, price)
+  end
+  arr
+end
